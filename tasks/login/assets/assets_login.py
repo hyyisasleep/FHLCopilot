fbfee116747947287a4ce05a33c7c8d4ab4ba264
hyyisasleep <huixiang1114@outlook.com>
2024-08-31 21:13:58 +0800

    0831

---
+++ tasks/login/assets/assets_login.py
@@ -0,0 +1,36 @@
+from module.base.button import Button, ButtonWrapper
+
+# This file was auto-generated, do not modify it manually. To generate:
+# ``` python -m dev_tools.button_extract ```
+
+LOGIN_CONFIRM = ButtonWrapper(
+    name='LOGIN_CONFIRM',
+    share=Button(
+        file='./assets/share/login/LOGIN_CONFIRM.png',
+        area=(1192, 50, 1246, 103),
+        search=(1172, 30, 1266, 123),
+        color=(101, 82, 76),
+        button=(832, 300, 1113, 565),
+    ),
+)
+LOGIN_LOADING = ButtonWrapper(
+    name='LOGIN_LOADING',
+    share=Button(
+        file='./assets/share/login/LOGIN_LOADING.png',
+        area=(1103, 599, 1119, 616),
+        search=(1083, 579, 1139, 636),
+        color=(98, 98, 106),
+        button=(1103, 599, 1119, 616),
+    ),
+)
+USER_AGREEMENT_ACCEPT = ButtonWrapper(
+    name='USER_AGREEMENT_ACCEPT',
+    cn=Button(
+        file='./assets/cn/login/USER_AGREEMENT_ACCEPT.png',
+        area=(751, 467, 791, 488),
+        search=(731, 447, 811, 508),
+        color=(196, 167, 111),
+        button=(678, 459, 867, 496),
+    ),
+    en=None,
+)
---
+++ tasks/rogue/keywords/bonus.py
@@ -0,0 +1,248 @@
+from .classes import RogueBonus
+
+# This file was auto-generated, do not modify it manually. To generate:
+# ``` python -m dev_tools.keyword_extract ```
+
+Fragmented_Universe = RogueBonus(
+    id=1,
+    name='Fragmented_Universe',
+    cn='破碎宇宙',
+    cht='破碎宇宙',
+    en='Fragmented Universe',
+    jp='破裂した宇宙',
+    es='Universo fragmentado',
+)
+Blessing_Universe = RogueBonus(
+    id=2,
+    name='Blessing_Universe',
+    cn='祝福宇宙',
+    cht='祝福宇宙',
+    en='Blessing Universe',
+    jp='祝福された宇宙',
+    es='Universo de bendiciones',
+)
+Miracle_Universe = RogueBonus(
+    id=3,
+    name='Miracle_Universe',
+    cn='神奇宇宙',
+    cht='神奇宇宙',
+    en='Miracle Universe',
+    jp='奇跡的な宇宙',
+    es='Universo milagroso',
+)
+Orderly_Universe = RogueBonus(
+    id=4,
+    name='Orderly_Universe',
+    cn='有序宇宙',
+    cht='有序宇宙',
+    en='Orderly Universe',
+    jp='秩序ある宇宙',
+    es='Universo ordenado',
+)
+Hungry_Universe = RogueBonus(
+    id=5,
+    name='Hungry_Universe',
+    cn='巨胃宇宙',
+    cht='巨胃宇宙',
+    en='Hungry Universe',
+    jp='大食いな宇宙',
+    es='Universo hambriento',
+)
+Bloodthirsty_Universe = RogueBonus(
+    id=6,
+    name='Bloodthirsty_Universe',
+    cn='嗜血宇宙',
+    cht='嗜血宇宙',
+    en='Bloodthirsty Universe',
+    jp='血を好む宇宙',
+    es='Universo chupasangre',
+)
+Inorganic_Universe = RogueBonus(
+    id=7,
+    name='Inorganic_Universe',
+    cn='无机宇宙',
+    cht='無機宇宙',
+    en='Inorganic Universe',
+    jp='無機の宇宙',
+    es='Universo inorgánico',
+)
+Equilibrium_Universe = RogueBonus(
+    id=8,
+    name='Equilibrium_Universe',
+    cn='均衡宇宙',
+    cht='均衡宇宙',
+    en='Equilibrium Universe',
+    jp='均衡な宇宙',
+    es='Universo equilibrado',
+)
+Path_of_Preservation = RogueBonus(
+    id=9,
+    name='Path_of_Preservation',
+    cn='存护之路',
+    cht='存護之路',
+    en='Path of Preservation',
+    jp='存護の道',
+    es='Camino de la Conservación',
+)
+Path_of_Remembrance = RogueBonus(
+    id=10,
+    name='Path_of_Remembrance',
+    cn='记忆之路',
+    cht='記憶之路',
+    en='Path of Remembrance',
+    jp='記憶の道',
+    es='Camino de la Reminiscencia',
+)
+Path_of_Nihility = RogueBonus(
+    id=11,
+    name='Path_of_Nihility',
+    cn='虚无之路',
+    cht='虛無之路',
+    en='Path of Nihility',
+    jp='虚無の道',
+    es='Camino de la Nihilidad',
+)
+Path_of_Abundance = RogueBonus(
+    id=12,
+    name='Path_of_Abundance',
+    cn='丰饶之路',
+    cht='豐饒之路',
+    en='Path of Abundance',
+    jp='豊穣の道',
+    es='Camino de la Abundancia',
+)
+Path_of_The_Hunt = RogueBonus(
+    id=13,
+    name='Path_of_The_Hunt',
+    cn='巡猎之路',
+    cht='巡獵之路',
+    en='Path of The Hunt',
+    jp='巡狩の道',
+    es='Camino de la Cacería',
+)
+Path_of_Destruction = RogueBonus(
+    id=14,
+    name='Path_of_Destruction',
+    cn='毁灭之路',
+    cht='毀滅之路',
+    en='Path of Destruction',
+    jp='壊滅の道',
+    es='Camino de la Destrucción',
+)
+Path_of_Elation = RogueBonus(
+    id=15,
+    name='Path_of_Elation',
+    cn='欢愉之路',
+    cht='歡愉之路',
+    en='Path of Elation',
+    jp='愉悦の道',
+    es='Camino de la Exultación',
+)
+Path_of_Propagation = RogueBonus(
+    id=16,
+    name='Path_of_Propagation',
+    cn='繁育之路',
+    cht='繁育之路',
+    en='Path of Propagation',
+    jp='繁殖の道',
+    es='Camino de la Propagación',
+)
+Path_of_Erudition = RogueBonus(
+    id=17,
+    name='Path_of_Erudition',
+    cn='智识之路',
+    cht='智識之路',
+    en='Path of Erudition',
+    jp='知恵の道',
+    es='Camino de la Erudición',
+)
+Coronal_Sea = RogueBonus(
+    id=18,
+    name='Coronal_Sea',
+    cn='日冕海',
+    cht='日冕海',
+    en='Coronal Sea',
+    jp='恒星コロナの海',
+    es='Mar de corona',
+)
+Skycruiser = RogueBonus(
+    id=19,
+    name='Skycruiser',
+    cn='巡天舰',
+    cht='巡天艦',
+    en='Skycruiser',
+    jp='巡天艦',
+    es='Nave surcacielos',
+)
+Diamond_Precipitation = RogueBonus(
+    id=20,
+    name='Diamond_Precipitation',
+    cn='钻石雨',
+    cht='鑽石雨',
+    en='Diamond Precipitation',
+    jp='ダイヤの雨',
+    es='Lluvia de diamantes',
+)
+Ivory_Tower = RogueBonus(
+    id=21,
+    name='Ivory_Tower',
+    cn='象牙塔',
+    cht='象牙塔',
+    en='Ivory Tower',
+    jp='象牙の塔',
+    es='Torre de marfil',
+)
+Fool_Play = RogueBonus(
+    id=22,
+    name='Fool_Play',
+    cn='愚人戏',
+    cht='愚人戲',
+    en="Fool's Play",
+    jp='愚者の芝居',
+    es='Teatro del absurdo',
+)
+Fountain_of_Youth = RogueBonus(
+    id=23,
+    name='Fountain_of_Youth',
+    cn='不老泉',
+    cht='不老泉',
+    en='Fountain of Youth',
+    jp='不老の泉',
+    es='Fuente de la juventud',
+)
+Reimbursement_Form = RogueBonus(
+    id=24,
+    name='Reimbursement_Form',
+    cn='报销单',
+    cht='報銷單',
+    en='Reimbursement Form',
+    jp='清算書',
+    es='Formulario de reembolso',
+)
+Sea_of_Forgetfulness = RogueBonus(
+    id=25,
+    name='Sea_of_Forgetfulness',
+    cn='遗忘海',
+    cht='遺忘海',
+    en='Sea of Forgetfulness',
+    jp='忘却の海',
+    es='Mar del olvido',
+)
+Phenomenal_Field = RogueBonus(
+    id=26,
+    name='Phenomenal_Field',
+    cn='现象场',
+    cht='現象場',
+    en='Phenomenal Field',
+    jp='現象の場',
+    es='Campo fenomenal',
+)
+Lying_Cap = RogueBonus(
+    id=27,
+    name='Lying_Cap',
+    cn='说谎帽',
+    cht='說謊帽',
+    en='Lying Cap',
+    jp='ペテンハット',
+    es='Sombrero mentiroso',
+)
